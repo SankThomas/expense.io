20d43238223fccf9b94e41ef1923b5a0ad55dba1
Thomas Sankara <thomassankara74@gmail.com>
2023-05-10 13:14:30 +0000

    add Sidebar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Expenses from "@/components/expenses";
 import Head from "next/head";
+import Sidebar from "@/components/sidebar";
 
 export default function App() {
   const { expense, setExpense, amount, setAmount, handleSubmitExpense } =
@@ -24,56 +25,59 @@ export default function App() {
         <meta name="author" content="Thomas Sankara, TsbSankara" />
         <meta name="keywords" content="expense tracker, expense.io, expensio" />
       </Head>
+
       <main>
-        <h1>Expense.io</h1>
+        <section className="max-w-[800px]">
+          <h1>Expense.io</h1>
 
-        <div className="expense">
+          <div className="expense">
+            <br />
+            <form onSubmit={handleSubmitExpense}>
+              <article>
+                <label htmlFor="expense">Add expense</label>
+                <input
+                  type="text"
+                  name="expense"
+                  id="expense"
+                  placeholder="Add expense"
+                  required
+                  value={expense}
+                  onChange={(e) => setExpense(e.target.value)}
+                />
+              </article>
+
+              <article>
+                <label htmlFor="amount">Add amount</label>
+                <input
+                  type="text"
+                  name="amount"
+                  id="amount"
+                  placeholder="Add amount"
+                  required
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+              </article>
+
+              <button
+                onClick={handleSubmitExpense}
+                type="submit"
+                className="btn-primary"
+              >
+                Add Expense
+              </button>
+
+              <ToastContainer
+                theme="colored"
+                hideProgressBar={true}
+                autoClose={3000}
+              />
+            </form>
+          </div>
+
           <br />
-          <form onSubmit={handleSubmitExpense}>
-            <article>
-              <label htmlFor="expense">Add expense</label>
-              <input
-                type="text"
-                name="expense"
-                id="expense"
-                placeholder="Add expense"
-                required
-                value={expense}
-                onChange={(e) => setExpense(e.target.value)}
-              />
-            </article>
-
-            <article>
-              <label htmlFor="amount">Add amount</label>
-              <input
-                type="text"
-                name="amount"
-                id="amount"
-                placeholder="Add amount"
-                required
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-              />
-            </article>
-
-            <button
-              onClick={handleSubmitExpense}
-              type="submit"
-              className="btn-primary"
-            >
-              Add Expense
-            </button>
-
-            <ToastContainer
-              theme="colored"
-              hideProgressBar={true}
-              autoClose={3000}
-            />
-          </form>
-        </div>
-
-        <br />
-        <Expenses />
+          <Expenses />
+        </section>
       </main>
     </>
   );
